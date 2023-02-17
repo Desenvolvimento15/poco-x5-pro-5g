d737ed5129939032b448a2f4adacb63a495d27c6
Herbert Ishimura Souza <desenvolvimento15@dl.com.br>
2023-02-17 16:13:29 -0300

    vs01

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $('.galeria').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-  });
+  dots: false,
+  infinite: true,
+  speed: 700,
+  fade: true,
+  autoplay: true,
+  cssEase: 'linear'
+});
